d4e8b37b0fe385cb2fee3e38d498f1acab39ba09
oliver138 <58395978+oliver138@users.noreply.github.com>
2022-09-22 20:13:19 +0800

    1
    1

---
+++ MWOS/resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    
+     
+    <!-- Fonts -->
+    
+   
+
+    <!-- Styles -->
+    
+</head>
+<body>
+
+    <div id="app">
+    
+   
+        <main class="">
+            @yield('content')
+        </main>
+
+    </div>
+</body>
+</html>
+
+
