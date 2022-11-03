fa582a92e35b85010293d172e746bbd398b256a6
oliver138 <58395978+oliver138@users.noreply.github.com>
2022-11-03 12:58:01 +0800

    v
    v

--- MWOS/app/Http/Controllers/CarpenterController.php
+++ MWOS/app/Http/Controllers/CarpenterController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class CarpenterController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth','verified']);
+    }
     public function dashboard()
     {  
         return view('carpenter.dashboardCarpenter');
