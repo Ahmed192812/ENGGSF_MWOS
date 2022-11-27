347f08572cca913802a4eda896c440597f9f8ed0
oliver138 <58395978+oliver138@users.noreply.github.com>
2022-11-27 20:13:15 +0800

    a
    a

--- MWOS/resources/views/admin/dashboardAdmin.blade.php
+++ MWOS/resources/views/admin/dashboardAdmin.blade.php
@@ -1,31 +1,219 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                <h4>Admin: {{Auth::user()->name}} role:{{Auth::user()->role}}</h4>
-                <hr>
-                Dashboard
-                <div class="card-body">
-                @if ($verifiedSuccess = Session::get('message'))
-                    <div class="alert alert-success alert-block">
-                        <button type="button" class="close" data-dismiss="alert">×</button>    
-                        <span>{{ $verifiedSuccess }} </span>
-                    </div>
-                 @endif
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
+  <!-- Content Row -->
+  <br>
+  <br>
+  <div class="row">
 
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-primary shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                       Customers</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<!-- Earnings (Monthly) Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                       Orders</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Earnings (Monthly) Card Example -->
+
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                       Products</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Pending Requests Card Example -->
+<div class="col-xl-3 col-md-6 mb-4">
+    <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                    Materials</div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
+                </div>
+                <div class="col-auto">
+                    <i class="fas fa-comments fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<br>
+<br>
+<br>
+<div class="row">
+    <div class="col-5">
+    <div class="card border-left-primary shadow">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+            <div class="container">
+    <div class="row justify-content-center">
+        <div class="">
+            <div class="card">
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <th scope="col">Lorem</th>
+      <th scope="col">Ipsum</th>
+      <th scope="col">Dolor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <td>Sit</td>
+      <td>Amet</td>
+      <td>Consectetur</td>
+    </tr>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <td>Adipisicing</td>
+      <td>Elit</td>
+      <td>Sint</td>
+    </tr>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+        </div>
+      </th>
+      <td>Hic</td>
+      <td>Fugiat</td>
+      <td>Temporibus</td>
+    </tr>
+  </tbody>
+</table>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+                <div class="col-auto">
+                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+</div>
+<div class="col-2"></div>
+<div class="col-5">
+    <div class="card border-left-primary shadow">
+        <div class="card-body">
+            <div class="row no-gutters align-items-center">
+            <div class="container">
+    <div class="row justify-content-center">
+        <div class="">
+            <div class="card">
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <th scope="col">Lorem</th>
+      <th scope="col">Ipsum</th>
+      <th scope="col">Dolor</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <td>Sit</td>
+      <td>Amet</td>
+      <td>Consectetur</td>
+    </tr>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+        </div>
+      </th>
+      <td>Adipisicing</td>
+      <td>Elit</td>
+      <td>Sint</td>
+    </tr>
+    <tr>
+      <th scope="row">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+        </div>
+      </th>
+      <td>Hic</td>
+      <td>Fugiat</td>
+      <td>Temporibus</td>
+    </tr>
+  </tbody>
+</table>
+            </div>
+        </div>
+    </div>
+</div>
+    </div>
+                <div class="col-auto">
+                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                </div>
+            </div>
+        </div>
+</div>
+
+    </div>
+    
 @endsection
